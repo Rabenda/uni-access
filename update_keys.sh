6b3b6c7de0b7364086b006dbfed7f083dc47159d
Han Gao <rabenda.cn@gmail.com>
2023-06-11 00:22:10 +0800

    Update update_keys.sh

--- update_keys.sh
+++ update_keys.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl https://raw.githubusercontent.com/Rabenda/uni-access/master/machine/nrv1.list | xargs ssh-import-id-gh
+curl https://raw.githubusercontent.com/Rabenda/uni-access/master/access.list | xargs ssh-import-id-gh
